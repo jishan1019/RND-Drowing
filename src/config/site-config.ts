96144342466a72bbe23be3dc7155cb750df6512e
jishan <imranislamjishan80@gmail.com>
2024-06-18 00:15:17 +0600

    some minor bug fix

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,7 +1,20 @@
-export const title = "Farmers Blogs";
-export const fullTitle = "Farmers Blogs - Fine farmer best solution";
-export const tagLine = "The Finest Blogs in the world";
-export const tagLine2 = "Generate idea for creating farmers";
+export const title = "Draw Dev Plus";
+export const fullTitle =
+  "Draw Dev Plus - Online Drawing and Development Platform";
+export const tagLine = "Unleash Your Creativity with Drawing and Development";
+export const tagLine2 =
+  "Explore, Create, and Share Your Drawings and Developments";
 export const description =
-  "Integrate diversity on your farm. Having different products in your farm ensures stability, sustainability, richer soil, enhance profits.";
-export const keywords = ["Farmers Blogs"];
+  "Draw Dev Plus is your ultimate online platform for drawing and development. Explore a wide range of creative tools, collaborate with others, and showcase your work.";
+export const keywords = [
+  "Draw Dev Plus",
+  "online drawing",
+  "development platform",
+  "creative tools",
+  "whiteboard",
+  "blackboard",
+  "canvas",
+  "artworks",
+  "design",
+  "rnd",
+];
